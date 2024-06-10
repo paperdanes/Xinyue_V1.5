74a9a42739be1ca27e665b8e1601ffefe98d2ede
纸丹 <334960656@qq.com>
2024-06-10 10:33:07 +0800

    6006接口

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
 
 #os.system("python download_model.py jujimeizuo/EmoLLM_Model")
-os.system('streamlit run web_demo-aiwei.py --server.address=0.0.0.0 --server.port 7860')
+os.system('streamlit run web_demo-aiwei.py --server.address=0.0.0.0 --server.port ‘6006')
