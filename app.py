dccb06c3c540975c8ff808991c399c5fbd9a95c0
纸丹 <334960656@qq.com>
2024-06-10 11:05:22 +0800

    7860

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import os
 
 #os.system("python download_model.py jujimeizuo/EmoLLM_Model")
-os.system('streamlit run web_demo-aiwei.py --server.address=0.0.0.0 --server.port 8080')
+os.system('streamlit run web_demo-aiwei.py --server.address=0.0.0.0 --server.port 7860')
